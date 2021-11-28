61672280cd658b0b73523d38f7a4c7ebcdbb0d6b
alan lima <alan.silva1f@gmail.com>
2021-11-28 14:24:48 -0300

    Desafios javascript

---
+++ desafio1-js/js/index.js
@@ -0,0 +1,6 @@
+
+let nomeUsuario = prompt();
+
+window.alert("Seja bem-vindo(a), " + nomeUsuario + " é um prezer lhe conhecer!");
+document.write("Seja bem-vindo(a), " + nomeUsuario + " é um prezer lhe conhecer!");
+console.log("Seja bem-vindo(a), " + nomeUsuario + " é um prezer lhe conhecer!");
