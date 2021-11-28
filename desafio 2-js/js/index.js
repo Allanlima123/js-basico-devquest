d3626dd3e6c936ac0a6f8e09b671bc8e9a8fcc48
alan lima <alan.silva1f@gmail.com>
2021-11-28 14:45:49 -0300

    Desafio 2 javaScript

---
+++ desafio 2-js/js/index.js
@@ -0,0 +1,9 @@
+let boletoPago = false;
+
+if(boletoPago == false){
+    alert("O boleto está pago!🤑");
+    console.log("O boleto está pago!🤑");
+}else{
+    alert("O boleto não está pago!😑");
+    console.log("O boleto não está pago!😑");
+}
